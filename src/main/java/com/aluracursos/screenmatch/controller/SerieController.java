38ac644ebc3e1a32ceb1fe0014f09fc78d0d3e27
Rene Jimenez <rene.jm10@gmail.com>
2024-05-19 21:32:45 -0600

    Se creo paquete controller y se agrego clase SerieController

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,4 +1,12 @@
 package com.aluracursos.screenmatch.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SerieController {
+    @GetMapping("/series")
+    public String mostrarMensaje() {
+        return "Serie de datos";
+    }
 }
